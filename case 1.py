9b1f470fbc5db9928c05c321562527b83c9b588d
Emirhan <kisaemirhan57@gmail.com>
2026-03-06 00:33:52 +0300

    Add files via upload

---
+++ case 1.py
@@ -0,0 +1,3 @@
+finans_verisi = "Hesabınıza 2500 TL yatırıldı. 150 EUR transfer edildi. 300 USD çekildi."
+
+# Beklenen çıktı ['2500 TL', '150 EUR', '300 USD']
---
+++ case 2.py
@@ -0,0 +1,3 @@
+# Görev : Aşağıdaki cümledeki farklı formatlarda yazılmış tarihleri bulup, hepsini gün.ay.yıl (01.01.2026) formatına çevirin.
+
+notlar = "Toplantı 15/03/2026 tarihinde yapılacak. Teslim tarihi ise 20-04-2026 olarak belirlendi."
